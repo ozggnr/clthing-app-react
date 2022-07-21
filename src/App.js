9de71220fc48d9f39c7057937323214f7772c280
ozggnr <ozgecaylioglu@gmail.com>
2022-07-21 11:09:08 -0400

    added auth page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.compoenent';
+import Authentication from './routes/authentication/authentication.component';
 
 function App() {
     return (
         <Routes>
             <Route path='/' element={<Navigation />}> {/**Outlet will render whatever inside this */}
                 <Route index element={<Home />} />
-                <Route path='sign-in' element={<SignIn />} />
+                <Route path='auth' element={<Authentication />} />
             </Route>
         </Routes>
     )
